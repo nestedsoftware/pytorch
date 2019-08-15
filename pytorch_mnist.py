b2c65454e882f7651f2a824a1840915d9e3946de
Nested Software <2969361+nestedsoftware@users.noreply.github.com>
2019-08-15 12:32:06 -0600

    refactored pytorch.mnist.py so that train and test can be reused generically

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -21,59 +21,33 @@ class Net(nn.Module):
         return x
 
 
-net = Net()
-
-
-def expand_expected_outputs(tensor_of_predicted_outputs):
-    return torch.tensor([expand(predicted_output.item())
-                         for predicted_output in tensor_of_predicted_outputs])
-
-
-def expand(predicted_output):
-    x = [0.0 for _ in range(OUTPUT_SIZE)]
-    x[predicted_output] = 1.0
-    return x
-
-# Remove channel and flatten images from 2-d to 1-d, i.e.
-# in this, case convert the tensor from size (10, 1, 28, 28)
-# to size (10, 784).
-# The first argument, `-1` is a placeholder whose value
-# is derived such that the total number of scalar values in
-# the tensor does not change.
-# Since we specify `input_size`, which is 28*28, or 784,
-# the first argument will become the number of batches,
-# which is 10 in this case.
-def reshape_inputs(inputs, input_size):
-    return inputs.view(-1, input_size)
-
-def train(data_loader, model, epochs, input_size, calc_loss, sgd, reshape):
+def train(data_loader, model, num_epochs, optimizer, reshape_input, calc_loss):
     num_batches = len(data_loader)
-    for epoch in range(epochs):
+    for epoch in range(num_epochs):
         for batch in enumerate(data_loader):
             i, (images, expected_outputs) = batch
 
-            images = reshape(images, input_size)
-
+            images = reshape_input(images)
             outputs = model(images)
-            loss = calc_loss(outputs, expand_expected_outputs(expected_outputs))
+            loss = calc_loss(outputs, expected_outputs)
 
             optimizer.zero_grad()
             loss.backward()
             sgd.step()
 
             if (i+1) % 100 == 0 or (i+1) == num_batches:
-                p = [epoch+1, epochs, i+1, num_batches, loss.item()]
+                p = [epoch+1, num_epochs, i+1, num_batches, loss.item()]
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(*p))
 
 
-def test(data_loader, model, input_size, reshape):
+def test(data_loader, model, reshape):
     with torch.no_grad():
         correct = 0
         total = 0
         for batch in data_loader:
             images, expected_outputs = batch
 
-            images = reshape(images, input_size)
+            images = reshape(images)
             outputs = model(images)
 
             # get the maximum value from each output in the batch
@@ -85,11 +59,51 @@ def test(data_loader, model, input_size, reshape):
         print(f"Test data results: {float(correct)/total}")
 
 
-criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(net.parameters(), lr=LEARNING_RATE)
-train(train_loader, net, NUM_EPOCHS, INPUT_SIZE, criterion, optimizer,
-      reshape_inputs)
+def expand_expected_output(tensor_of_predicted_outputs, output_size):
+    return torch.tensor([expand(predicted_output.item(), output_size)
+                         for predicted_output in tensor_of_predicted_outputs])
+
+
+def expand(predicted_output, output_size):
+    x = [0.0 for _ in range(output_size)]
+    x[predicted_output] = 1.0
+    return x
+
+
+def reshape_inputs(inputs, input_size):
+    # Remove channel and flatten images from 2-d to 1-d, i.e.
+    # assuming batch size of 10, convert the tensor from
+    # size (10, 1, 28, 28) to size (10, 784).
+    # The first argument, `-1` is a placeholder whose value
+    # is derived such that the total number of scalar values in
+    # the tensor does not change.
+    # Since we specify `input_size`, which is 28*28, or 784,
+    # the first argument will become the number of batches,
+    # which is 10 in this case.
+    return inputs.view(-1, input_size)
+
+
+def create_input_reshaper(input_size=INPUT_SIZE):
+    def reshape(images):
+        return reshape_inputs(images, input_size)
+    return reshape
+
+
+def create_loss_calculator(loss_function, output_size=OUTPUT_SIZE):
+    def calc_loss(outputs, target):
+        targets = expand_expected_output(target, output_size)
+        return loss_function(outputs, targets)
+    return calc_loss
+
+
+net = Net()
+mse_loss_function = nn.MSELoss()
+sgd = torch.optim.SGD(net.parameters(), lr=LEARNING_RATE)
+
+train(train_loader, net, NUM_EPOCHS, sgd,
+      create_input_reshaper(),
+      create_loss_calculator(mse_loss_function))
 
 print("")
 
-test(test_loader, net, INPUT_SIZE, reshape_inputs)
+test(test_loader, net, create_input_reshaper())
